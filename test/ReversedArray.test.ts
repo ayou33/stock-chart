de69e4fe567c65310f02956ecd9f14332df72ea8
阿佑 <ayooooo@petalmail.com>
2022-08-01 11:15:15 +0800

    test support

---
+++ test/ReversedArray.test.ts
@@ -0,0 +1,12 @@
+/**
+ *  ReversedArray.ts of project stock-chart
+ *  @date 2022/8/1 11:09
+ *  @author 阿佑[ayooooo@petalmail.com]
+ */
+import ReversedArray from '../src/core/ReversedArray'
+
+describe('ReversedArray', () => {
+  const a = new ReversedArray([])
+
+  expect(a).toBe(1)
+})
